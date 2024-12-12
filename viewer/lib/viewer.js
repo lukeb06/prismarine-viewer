cfc552f905da654b5d7b4ff04cc39b01a0593e79
Luke Barrier <lukebbarrier06@outlook.com>
2024-12-12 00:45:18 -0500

    Update

--- viewer/lib/viewer.js
+++ viewer/lib/viewer.js
@@ -7,120 +7,121 @@ const { getVersion } = require('./version')
 const { Vec3 } = require('vec3')
 
 class Viewer {
-  constructor (renderer) {
-    this.scene = new THREE.Scene()
-    this.scene.background = new THREE.Color('lightblue')
+    constructor(renderer) {
+        this.scene = new THREE.Scene()
+        this.scene.background = new THREE.Color('lightblue')
 
-    this.ambientLight = new THREE.AmbientLight(0xcccccc)
-    this.scene.add(this.ambientLight)
+        this.ambientLight = new THREE.AmbientLight(0xcccccc)
+        this.scene.add(this.ambientLight)
 
-    this.directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
-    this.directionalLight.position.set(1, 1, 0.5).normalize()
-    this.directionalLight.castShadow = true
-    this.scene.add(this.directionalLight)
+        this.directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+        this.directionalLight.position.set(1, 1, 0.5).normalize()
+        this.directionalLight.castShadow = true
+        this.scene.add(this.directionalLight)
 
-    const size = renderer.getSize(new THREE.Vector2())
-    this.camera = new THREE.PerspectiveCamera(75, size.x / size.y, 0.1, 1000)
+        const size = renderer.getSize(new THREE.Vector2())
+        this.camera = new THREE.PerspectiveCamera(75, size.x / size.y, 0.1, 1000)
 
-    this.world = new WorldRenderer(this.scene)
-    this.entities = new Entities(this.scene)
-    this.primitives = new Primitives(this.scene, this.camera)
+        this.world = new WorldRenderer(this.scene)
+        this.entities = new Entities(this.scene)
+        this.primitives = new Primitives(this.scene, this.camera)
 
-    this.domElement = renderer.domElement
-    this.playerHeight = 1.6
-    this.isSneaking = false
-  }
-
-  resetAll () {
-    this.world.resetWorld()
-    this.entities.clear()
-    this.primitives.clear()
-  }
-
-  setVersion (version) {
-    version = getVersion(version)
-    if (version === null) {
-      const msg = `${version} is not supported`
-      window.alert(msg)
-      console.log(msg)
-      return false
+        this.domElement = renderer.domElement
+        this.playerHeight = 1.6
+        this.isSneaking = false
     }
-    console.log('Using version: ' + version)
-    this.version = version
-    this.world.setVersion(version)
-    this.entities.clear()
-    this.primitives.clear()
-    return true
-  }
 
-  addColumn (x, z, chunk) {
-    this.world.addColumn(x, z, chunk)
-  }
-
-  removeColumn (x, z) {
-    this.world.removeColumn(x, z)
-  }
-
-  setBlockStateId (pos, stateId) {
-    this.world.setBlockStateId(pos, stateId)
-  }
-
-  updateEntity (e) {
-    this.entities.update(e)
-  }
-
-  updatePrimitive (p) {
-    this.primitives.update(p)
-  }
-
-  setFirstPersonCamera (pos, yaw, pitch) {
-    if (pos) {
-      let y = pos.y + this.playerHeight
-      if (this.isSneaking) y -= 0.3
-      new TWEEN.Tween(this.camera.position).to({ x: pos.x, y, z: pos.z }, 50).start()
+    resetAll() {
+        this.world.resetWorld()
+        this.entities.clear()
+        this.primitives.clear()
     }
-    this.camera.rotation.set(pitch, yaw, 0, 'ZYX')
-  }
 
-  listen (emitter) {
-    emitter.on('entity', (e) => {
-      this.updateEntity(e)
-    })
+    setVersion(version) {
+        version = '1.20.1';
+        version = getVersion(version)
+        if (version === null) {
+            const msg = `${version} is not supported`
+            window.alert(msg)
+            console.log(msg)
+            return false
+        }
+        console.log('Using version: ' + version)
+        this.version = version
+        this.world.setVersion(version)
+        this.entities.clear()
+        this.primitives.clear()
+        return true
+    }
 
-    emitter.on('primitive', (p) => {
-      this.updatePrimitive(p)
-    })
+    addColumn(x, z, chunk) {
+        this.world.addColumn(x, z, chunk)
+    }
 
-    emitter.on('loadChunk', ({ x, z, chunk }) => {
-      this.addColumn(x, z, chunk)
-    })
+    removeColumn(x, z) {
+        this.world.removeColumn(x, z)
+    }
 
-    emitter.on('unloadChunk', ({ x, z }) => {
-      this.removeColumn(x, z)
-    })
+    setBlockStateId(pos, stateId) {
+        this.world.setBlockStateId(pos, stateId)
+    }
 
-    emitter.on('blockUpdate', ({ pos, stateId }) => {
-      this.setBlockStateId(new Vec3(pos.x, pos.y, pos.z), stateId)
-    })
+    updateEntity(e) {
+        this.entities.update(e)
+    }
 
-    this.domElement.addEventListener('pointerdown', (evt) => {
-      const raycaster = new THREE.Raycaster()
-      const mouse = new THREE.Vector2()
-      mouse.x = (evt.clientX / this.domElement.clientWidth) * 2 - 1
-      mouse.y = -(evt.clientY / this.domElement.clientHeight) * 2 + 1
-      raycaster.setFromCamera(mouse, this.camera)
-      const ray = raycaster.ray
-      emitter.emit('mouseClick', { origin: ray.origin, direction: ray.direction, button: evt.button })
-    })
-  }
+    updatePrimitive(p) {
+        this.primitives.update(p)
+    }
 
-  update () {
-    TWEEN.update()
-  }
+    setFirstPersonCamera(pos, yaw, pitch) {
+        if (pos) {
+            let y = pos.y + this.playerHeight
+            if (this.isSneaking) y -= 0.3
+            new TWEEN.Tween(this.camera.position).to({ x: pos.x, y, z: pos.z }, 50).start()
+        }
+        this.camera.rotation.set(pitch, yaw, 0, 'ZYX')
+    }
 
-  async waitForChunksToRender () {
-    await this.world.waitForChunksToRender()
-  }
+    listen(emitter) {
+        emitter.on('entity', (e) => {
+            this.updateEntity(e)
+        })
+
+        emitter.on('primitive', (p) => {
+            this.updatePrimitive(p)
+        })
+
+        emitter.on('loadChunk', ({ x, z, chunk }) => {
+            this.addColumn(x, z, chunk)
+        })
+
+        emitter.on('unloadChunk', ({ x, z }) => {
+            this.removeColumn(x, z)
+        })
+
+        emitter.on('blockUpdate', ({ pos, stateId }) => {
+            this.setBlockStateId(new Vec3(pos.x, pos.y, pos.z), stateId)
+        })
+
+        this.domElement.addEventListener('pointerdown', (evt) => {
+            const raycaster = new THREE.Raycaster()
+            const mouse = new THREE.Vector2()
+            mouse.x = (evt.clientX / this.domElement.clientWidth) * 2 - 1
+            mouse.y = -(evt.clientY / this.domElement.clientHeight) * 2 + 1
+            raycaster.setFromCamera(mouse, this.camera)
+            const ray = raycaster.ray
+            emitter.emit('mouseClick', { origin: ray.origin, direction: ray.direction, button: evt.button })
+        })
+    }
+
+    update() {
+        TWEEN.update()
+    }
+
+    async waitForChunksToRender() {
+        await this.world.waitForChunksToRender()
+    }
 }
 
 module.exports = { Viewer }
