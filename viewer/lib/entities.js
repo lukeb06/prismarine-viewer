a20f279c361de94df24bd2a362b0a830f703e930
Luke Barrier <lukebbarrier06@outlook.com>
2024-12-13 00:59:35 -0500

    Update

--- viewer/lib/entities.js
+++ viewer/lib/entities.js
@@ -4,15 +4,21 @@ const TWEEN = require('@tweenjs/tween.js')
 const Entity = require('./entity/Entity')
 const { dispose3 } = require('./dispose')
 
-const { createCanvas } = require('canvas')
-
 function getEntityMesh(entity, scene) {
     if (entity.name) {
         try {
             const e = new Entity('1.16.4', entity.name, scene)
 
             if (entity.username !== undefined) {
-                const canvas = createCanvas(500, 100)
+                let canvas;
+                if (typeof window === 'undefined') {
+                    const { createCanvas } = require('canvas')
+                    canvas = createCanvas(500, 100);
+                } else {
+                    canvas = document.createElement('canvas');
+                    canvas.width = 500;
+                    canvas.height = 100;
+                }
 
                 const ctx = canvas.getContext('2d')
                 ctx.font = '50pt Arial'
